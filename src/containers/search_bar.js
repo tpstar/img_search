b75cd08e039442b009e626b94e3fcddd29d9bf6d
tpstar <hanlee12@gmail.com>
2018-05-10 11:36:08 -0500

    add search bar component

---
+++ src/containers/search_bar.js
@@ -0,0 +1,41 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+
+
+class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: "" };
+  }
+
+  handleOnChange = event => {
+    this.setState({term: event.target.value})
+  }
+
+  handleOnSubmit = event => {
+    event.preventDefault(); // prevent the page from refreshing
+    // fetch pictures here
+    console.log(this.state.term);
+    this.setState({term: ""}) // initialize the search term
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleOnSubmit} className="input-group">
+        <input
+          placeholder="Search"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.handleOnChange}
+        />
+        <span className="input-group-btn">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
+      </form>
+    );
+  }
+}
+
+
+export default SearchBar;
