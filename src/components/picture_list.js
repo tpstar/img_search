4be6483ad5036e0a124a300aa043fc117632a606
tpstar <hanlee12@gmail.com>
2018-05-12 12:21:09 -0500

    implement masonry layout using grid layout and componentDidUdate

--- src/components/picture_list.js
+++ src/components/picture_list.js
@@ -3,21 +3,38 @@ import { connect } from "react-redux";
 
 class PictureList extends Component {
 
+  resizeGridItem(item){
+     let grid = document.getElementsByClassName("grid")[0];
+     let rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+     let rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+     let rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+     item.style.gridRowEnd = "span "+rowSpan;
+  }
+
+  componentDidUpdate() {
+    let allItems = document.getElementsByClassName("item");
+    for(let x=0; x<allItems.length ;x++){
+       this.resizeGridItem(allItems[x]);
+    }
+  }
+
   renderPictures(pictureData) {
 
     const {title, link, media } = pictureData;
 
     return (
-      <figure key={link}>
-        <img src={media.m} alt={title} className="picture"/>
-        <figcaption>{title}</figcaption>
-      </figure>
+      <div className="item" ref="item" key={link}>
+        <div className="content">
+          <img src={media.m} alt={title} className="picture" ref="picture"/>
+          <p>{title}</p>
+        </div>
+      </div>
     );
   }
 
   render() {
     return (
-      <div>
+      <div className="grid" ref="grid">
         {this.props.pictures.map(this.renderPictures)}
       </div>
     );
---
+++ src/grid_layout.js
@@ -0,0 +1,29 @@
+export const gridLayout = () => {
+  function resizeGridItem(item){
+     grid = document.getElementsByClassName("grid")[0];
+     rowHeight = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-auto-rows'));
+     rowGap = parseInt(window.getComputedStyle(grid).getPropertyValue('grid-row-gap'));
+     rowSpan = Math.ceil((item.querySelector('.content').getBoundingClientRect().height+rowGap)/(rowHeight+rowGap));
+     item.style.gridRowEnd = "span "+rowSpan;
+  }
+
+  function resizeAllGridItems(){
+     allItems = document.getElementsByClassName("item");
+     for(x=0;x<allItems.length;x++){
+        resizeGridItem(allItems[x]);
+     }
+  }
+
+  window.onload = resizeAllGridItems();
+  window.addEventListener("resize", resizeAllGridItems);
+
+  allItems = document.getElementsByClassName("item"); //to calculate height of images when the image is fully loaded
+  for(x=0;x<allItems.length;x++){
+     imagesLoaded( allItems[x], resizeInstance);
+  }
+
+  function resizeInstance(instance){
+     item = instance.elements[0];
+     resizeGridItem(item);
+  }
+}
